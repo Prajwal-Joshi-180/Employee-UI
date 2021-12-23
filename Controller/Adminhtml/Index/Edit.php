1a801e9ee459c1bd7a9a9813bdf2b73bf7a01b19
Prajwal Joshi <prajwal.j@codilar.com>
2021-12-23 23:07:44 +0530

    validations added

--- Controller/Adminhtml/Index/Edit.php
+++ Controller/Adminhtml/Index/Edit.php
@@ -4,6 +4,8 @@ namespace Codilar\HelloWorld\Controller\Adminhtml\Index;
 
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
+use Codilar\Employee\Model\EmployeeFactory as ModelFactory;
+use Codilar\Employee\Model\ResourceModel\Employee as ResourceModel;
 
 class Edit extends \Magento\Backend\App\Action
 {
@@ -11,19 +13,38 @@ class Edit extends \Magento\Backend\App\Action
      * @var PageFactory
      */
     private $pageFactory;
+     /**
+     * @var ModelFactory
+     */
+    protected $modelFactory;
+
+    /**
+     * @var ResourceModel
+     */
+    protected $resourceModel;
+
 
     public function __construct(
         Context $context,
-        PageFactory $pageFactory
+        PageFactory $pageFactory, 
+        ModelFactory $modelFactory,
+        ResourceModel $resourceModel
     )
     {
         
         $this->pageFactory = $pageFactory;
+        $this->modelFactory = $modelFactory;
+        $this->resourceModel = $resourceModel;
         parent::__construct($context);
     }
 
     public function execute()
     {
-        return $this->pageFactory->create();
+        $page=$this->pageFactory->create();
+        $data=$this->getRequest()->getParams();
+        $Employee = $this->modelFactory->create();
+        $Employee->load($data['entity_id']);
+        $page->getConfig()->getTitle()->set('Employee '. $Employee->getName()."'s Details");
+        return $page;
     }
 }
